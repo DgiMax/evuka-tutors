40813d148183f7d3185e39227fd8e01ebb5c49b4
Leapfrog User <jamesmatatamule@gmail.com>
2025-11-29 01:10:47 +0300

    ------------------------

--- src/lib/api/axios.ts
+++ src/lib/api/axios.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://api.e-vuka.com",
   withCredentials: true,
   headers: {
     Accept: "application/json",
