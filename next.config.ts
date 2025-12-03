80dd169e7023b6c719ff7e1f52d16aa6606c022f
Leapfrog User <jamesmatatamule@gmail.com>
2025-12-03 12:35:06 +0300

    ------------

--- next.config.ts
+++ next.config.ts
@@ -1,45 +1,47 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-  reactStrictMode: true,
+  output: "standalone",
+  reactStrictMode: true,
 
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "storage.e-vuka.com",
-        port: "",
-        pathname: "/**",
-      },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.e-vuka.com",
+        port: "",
+        pathname: "/**",
+      },
 
-      {
-        protocol: "http",
-        hostname: "minio",
-        port: "9000",
-        pathname: "/**",
-      },
+      {
+        protocol: "https",
+        hostname: "api.e-vuka.com",
+        port: "",
+        pathname: "/media/**",
+      },
 
-      {
-        protocol: "http",
-        hostname: "127.0.0.1",
-        port: "",
-        pathname: "/e-vuka/**",
-      },
-      {
-        protocol: "http",
-        hostname: "127.0.0.1",
-        port: "",
-        pathname: "/media/**",
-      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+        pathname: "/media/**",
+      },
 
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "", 
+        pathname: "/media/**",
+      },
+
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+        pathname: "/**",
+      },
+        
+    ],
+  },
 };
 
 module.exports = nextConfig;
